f9c3326762976412218c59c3fed22fd81c307131
JANNA RIZZA RIZZA <>
2022-04-02 02:40:59 +0000

    init

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
 
+import streamlit as st
+import pandas as pd
+
+# page expands to full width
+st.set_page_config(page_title="LSTM vs ARIMA", layout='wide')
+
+
+# PAGE LAYOUT
+# heading
+st.title("Brent Crude Oil Benchmark")
+st.write("""Yaharooo""")
+
+# sidebar
+
+# model
